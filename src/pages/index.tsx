ba0630305317b36a4bf9cb5b395613fc59a03d27
toannv <toannv@newwave.vn>
2023-11-16 18:01:58 +0700

    products component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Banner from "@/components/Banner";
+import Products from "@/components/Products";
+import { ProductProps } from "../../type";
 
-export default function Home() {
+interface Props {
+  productData: ProductProps;
+}
+
+export default function Home({ productData }: Props) {
   return (
     <main>
       <div className="max-w-screen-2xl mx-auto">
         <Banner />
+        <div className="relative md:-mt020 lgl:-mt-32 xl:-mt-60 z-20 mb-10">
+          <Products productData={productData} />
+        </div>
       </div>
     </main>
   );
 }
+
+// SSR
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return { props: { productData } };
+};
