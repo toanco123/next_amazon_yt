da62c88264e63b1477a110e479000a56c03a4fcc
toannv <toannv@newwave.vn>
2023-11-16 16:43:14 +0700

    banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Banner from "@/components/Banner";
+
 export default function Home() {
   return (
     <main>
-      <div className="py-10 bg-gray-300 "> this is home page</div>
+      <div className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </div>
     </main>
   );
 }
